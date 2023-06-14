12228b0df601be84cd9bb943be88385eaa2ea5f6
Egor Gasanov <Gasanov.EV11@ya.ru>
2023-06-14 17:42:10 +0300

    Add Example005_ConditionIfElse

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear ();
+Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "kate")
+{
+    Console.WriteLine ("Ура! Это же Kate!");
+}
+else
+{
+    Console.WriteLine("Привет,");
+    Console.WriteLine(username);
+         
+}
