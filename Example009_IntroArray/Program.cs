c27bbd34423c0715369f79672bac5bdbe1d26883
Egor Gasanov <Gasanov.EV11@ya.ru>
2023-06-20 16:13:56 +0300

    Example009_IntroArray

---
+++ Example009_IntroArray/Program.cs
@@ -0,0 +1,23 @@
+﻿int Max(int arg1, int arg2, int arg3 )
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+int [] array = {11,21,31,41,15,61,17,18,19};
+
+
+
+//int max1 = Max(a1, b1, c1);
+//int max2 = Max(a2, b2, c2);
+//int max3 = Max(a3, b3, c3);
+//int max = Max(max1, max2, max3);
+
+int max = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8]));
+
+Console.WriteLine (max);
+
